74cfa1abbc269c96d0e218ed747d43402b9ad5bc
Mitja Desmet <mitja.desmet@vub.be>
2021-10-15 16:03:24 +0200

    Reproduced Davids normalisation

--- Amplitude spectrum fitting/SliceFit.py
+++ Amplitude spectrum fitting/SliceFit.py
@@ -5,6 +5,11 @@ from scipy.optimize import curve_fit
 from scipy.constants import c as c_vacuum
 
 
+def get_number_of_particles(path, slice):
+    long = np.genfromtxt(path, skip_header=2, skip_footer=216, usecols=(2,3))
+    return np.sum(long[int(slice/5 + 1), :])
+
+
 def amplitude_fun_slice(f, a_0, b, c, d):
     return a_0 * np.exp(b * f + c * f ** 2) + d
 
@@ -12,7 +17,7 @@ def amplitude_fun_slice(f, a_0, b, c, d):
 def amplitude_fit_slice(fdata, ydata, x, r):
     d = max(0, 1e-9 * (x/400 - 1.5) * np.exp(1 - r/40000))
     try:
-        popt, pcov = curve_fit(lambda f, a_0, b, c: a_0 * np.exp(b * f + c * f ** 2), fdata / 1e6, ydata - d ** 2,
+        popt, pcov = curve_fit(lambda f, a_0, b, c: a_0 * np.exp(b * f + c * f ** 2) + d**2, fdata / 1e6, ydata,
                                p0=[ydata[0], -2.5e-3, 0], bounds=(-np.inf, [np.inf, 1e-2, 1e-3]),
                                sigma=np.maximum(ydata*0.1, np.maximum(d**2, 1e-18)), maxfev=10000)
     except RuntimeError:
@@ -23,17 +28,19 @@ def amplitude_fit_slice(fdata, ydata, x, r):
 
 
 distances = [1, 4000, 7500, 11000, 15000, 37500]
-files_path = '/mnt/hgfs/Shared data/BulkSynth/bulksynth-7/'
-sim = 'SIM100002'  # shower with Xmax = 862 g/cm2
+files_path = '/mnt/hgfs/Shared data/BulkSynth/bulksynth-18/'
+david_path = '/mnt/hgfs/Shared data/ampfitQ/'
+sim = '110002'
 antenna = 3
 Xslice = 655
 
-with open(files_path+sim+f'_coreas/raw_{antenna}x{Xslice}.dat', 'r') as file:
+with open(files_path+'SIM'+sim+f'_coreas/raw_{antenna}x{Xslice}.dat', 'r') as file:
     data = np.genfromtxt(file) * c_vacuum * 1e2  # conversion from statV/cm to microV/m
 
 freq = np.fft.rfftfreq(len(data), 2e-10)
 frange = np.logical_and(12 * 1e6 <= freq, freq <= 502 * 1e6)
 
+Nslice = get_number_of_particles(files_path+'DAT' + sim + '.long', Xslice)
 spectrum = np.apply_along_axis(np.fft.rfft, 0, data[:, 1:])
 amplitude = np.abs(spectrum)  # Need to multiply by 2 because 1-sided FT
 filtered = amplitude[frange]
@@ -55,28 +62,38 @@ coefY, dY = amplitude_fit_slice(freq[frange],  filtered[:, 1], Xslice, distances
 
 print('The x component has parameters', *coefX)
 print('The y component has parameters', *coefY)
+''' 
+david_data = np.genfromtxt(david_path + sim + 'ampfit_Q.txt')
+david_data = david_data.reshape((207, 6, 2, 3))
 
+print('David x component has parameters', *david_data[int(Xslice/5 - 1), antenna, 0, :])
+print('David y component has parameters', *david_data[int(Xslice/5 - 1), antenna, 1, :])
+'''
 fig = plt.figure()
 
 ax = fig.add_subplot(121)
 ax.plot(freq[frange]/1e6,  filtered[:, 0], linestyle='--', color='orange')
 ax.plot(freq[frange]/1e6, amplitude_fun_slice(freq[frange]/1e6, (coefX[0]), coefX[1], coefX[2], dX),
-        color='orange')
-# ax.plot((freq[filter])/1e6, amplitude_fun_slice(freq[filter]-2.5*1e8, np.exp(coef0X[0]), coef0X[1], coef0X[2], d0X),
-#         linestyle='dotted', color='orange')
+        color='orange', label='My fit')
+# ax.plot(freq[frange]/1e6, amplitude_fun_slice(freq[frange]/1e6, *david_data[int(Xslice/5 - 1), antenna, 0, :], dX-0.11e-17),
+#         linestyle='dotted', color='black', label='David result')
 ax.set_xlabel('Frequency (MHz)')
 #ax.set_yscale('log')
 ax.set_title('The x-component')
 ax.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
 # ax.ticklabel_format(scilimits=(-10, -10), axis='y')
+ax.legend()
 
 ax2 = fig.add_subplot(122)
 ax2.plot(freq[frange]/1e6,  filtered[:, 1], linestyle='--', color='blue')
 ax2.plot(freq[frange]/1e6, amplitude_fun_slice(freq[frange]/1e6, (coefY[0]), coefY[1], coefY[2], dY),
-         color='blue')
+         color='blue', label='My fit')
+# ax2.plot(freq[frange]/1e6, amplitude_fun_slice(freq[frange]/1e6, *david_data[int(Xslice/5 - 1), antenna, 1, :], dY-0.23e-17),
+#          linestyle='dotted', color='black', label='David result')
 ax2.set_xlabel('Frequency (MHz)')
 ax2.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
 # ax2.ticklabel_format(scilimits=(-10, -10), axis='y')
 ax2.set_title('The y-component')
+ax2.legend()
 
 plt.show()
