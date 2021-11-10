9c5448c4d5d0853f2694f74e774360ed169501d0
Mitja Desmet <mitja.desmet@vub.be>
2021-11-10 15:41:51 +0100

    Compared synthesized pulse to target - not working

--- Amplitude spectrum fitting/PrepareTemplate.py
+++ Amplitude spectrum fitting/PrepareTemplate.py
@@ -1,10 +1,12 @@
 import os
+import glob
 import numpy as np
 from scipy.constants import c as c_vacuum
 
 SIM_DIRECTORY = '/mnt/hgfs/Shared data/BulkSynth/bulksynth-17/'
-PARAM_DIRECTORY = '/home/mdesmet/PycharmProjects/cr-template-synthesis/Amplitude spectrum fitting/paramFitCenter/'
+PARAM_DIRECTORY = '/home/mdesmet/PycharmProjects/cr-template-synthesis/Amplitude spectrum fitting/paramFit50/'
 TEMPLATE_NR = '100052'
+TARGET_NR = '200013'
 
 
 def get_number_of_particles(path):
@@ -24,7 +26,7 @@ def get_number_of_particles(path):
         return res
 
 
-# Infer the number of slices and number of antennas present in the simulation
+# Infer the number of slices and number of antennas present in the template simulation
 sim_dir = os.path.join(SIM_DIRECTORY, f'SIM{TEMPLATE_NR}_coreas/')
 particles_slice = get_number_of_particles(os.path.join(SIM_DIRECTORY, f'DAT{TEMPLATE_NR}.long'))
 n_slice = particles_slice.shape[-1]
@@ -39,6 +41,14 @@ with open(os.path.join(SIM_DIRECTORY, 'SIM' + TEMPLATE_NR + '.reas'), 'r') as fi
             primaryE = float(line.split()[2])
 x_max_vector = np.array([1, x_max, x_max**2])
 
+# Target shower parameters
+particles_target = get_number_of_particles(os.path.join(SIM_DIRECTORY, f'DAT{TARGET_NR}.long'))
+with open(os.path.join(SIM_DIRECTORY, 'SIM' + TARGET_NR + '.reas'), 'r') as file:
+    for line in file:
+        if 'DepthOfShowerMaximum' in line:
+            x_max = float(line.split()[2])
+x_max_vector_target = np.array([1, x_max, x_max**2])
+
 # Find the frequency range in consideration, more specifically the number of frequencies and the number of polarizations
 with open(os.path.join(sim_dir, f'raw_0x5.dat'), 'r') as file:
     data = np.genfromtxt(file) * c_vacuum * 1e2
@@ -51,24 +61,29 @@ n_pol = 2
 A = np.zeros((n_antenna, n_slice, n_freq, n_pol))
 Phi = np.zeros((n_antenna, n_slice, n_freq, n_pol))
 A_temp = np.zeros((n_antenna, n_slice, n_freq, n_pol))
+A_synth = np.zeros((n_antenna, n_slice, n_freq, n_pol))
+
 A0 = np.zeros((n_antenna, n_slice, n_pol))
 b = np.zeros((n_antenna, n_slice, n_pol))
 c = np.zeros((n_antenna, n_slice, n_pol))
 
+A0_synth = np.zeros((n_antenna, n_slice, n_pol))
+b_synth = np.zeros((n_antenna, n_slice, n_pol))
+c_synth = np.zeros((n_antenna, n_slice, n_pol))
+
 # Analyze all the files of the template shower
 os.chdir(sim_dir)
 for slice_nr in range(n_slice):
-    for antenna in range(n_antenna):
-        with open(f'raw_{antenna}x{int((slice_nr + 1) * 5)}.dat', 'r') as file:
+    for antenna_nr in range(n_antenna):
+        with open(f'raw_{antenna_nr}x{int((slice_nr + 1) * 5)}.dat', 'r') as file:
             data = np.genfromtxt(file) * c_vacuum * 1e2
 
         spectrum = np.apply_along_axis(np.fft.rfft, 0, data[:, 1:], norm='forward')
         amplitude = np.abs(spectrum) * 2  # Need to multiply by 2 because 1-sided FT
         phase = np.angle(spectrum)
-        filtered = amplitude[f_range]
 
-        A[antenna, slice_nr, :, :] = filtered[:, :2]
-        Phi[antenna, slice_nr, :, :] = phase[f_range][:, :2]
+        A[antenna_nr, slice_nr, :, :] = amplitude[f_range][:, :2]
+        Phi[antenna_nr, slice_nr, :, :] = phase[f_range][:, :2]
 
     with open(os.path.join(PARAM_DIRECTORY, 'fitX', f'slice{int((slice_nr + 1) * 5)}.dat'), 'r') as fileX, \
             open(os.path.join(PARAM_DIRECTORY, 'fitY', f'slice{int((slice_nr + 1) * 5)}.dat'), 'r') as fileY:
@@ -79,16 +94,49 @@ for slice_nr in range(n_slice):
         p_b_x, p_b_y = dataX[:, 4:7], dataY[:, 4:7]
         p_c_x, p_c_y = dataX[:, 7:], dataY[:, 7:]
 
+        if slice_nr == 90:
+            print("Here")
+
         A0[:, slice_nr, 0], A0[:, slice_nr, 1] = np.matmul(p_A0_x, x_max_vector), np.matmul(p_A0_y, x_max_vector)
         b[:, slice_nr, 0], b[:, slice_nr, 1] = np.matmul(p_b_x, x_max_vector), np.matmul(p_b_y, x_max_vector)
-        c[:, slice_nr, 0], c[:, slice_nr, 1] = np.matmul(p_c_x, x_max_vector), np.matmul(p_c_x, x_max_vector)
+        c[:, slice_nr, 0], c[:, slice_nr, 1] = np.matmul(p_c_x, x_max_vector), np.matmul(p_c_y, x_max_vector)
+
+        A0_synth[:, slice_nr, 0], A0_synth[:, slice_nr, 1] = \
+            np.matmul(p_A0_x, x_max_vector_target), np.matmul(p_A0_y, x_max_vector_target)
+        b_synth[:, slice_nr, 0], b_synth[:, slice_nr, 1] = \
+            np.matmul(p_b_x, x_max_vector_target), np.matmul(p_b_y, x_max_vector_target)
+        c_synth[:, slice_nr, 0], c_synth[:, slice_nr, 1] = \
+            np.matmul(p_c_x, x_max_vector_target), np.matmul(p_c_y, x_max_vector_target)
 
 # Calculate normalization factors, with frequency in MHz
 for i, f in enumerate(freq[f_range] / 1e6):
-    A_temp[:, :, i, :] = A0 * np.exp(b * f) * np.exp(c * f**2)  # have to add d to this
+    A_temp[:, :, i, :] = A0 * np.exp(b * f + c * f ** 2)  # have to add d to this
+    A_synth[:, :, i, :] = A0_synth * np.exp(b_synth * f + c_synth * f ** 2)
 
-A_temp[np.where(A_temp == 0)] = 1  # Avoid division by 0, due to overflow in exp
+A_temp[np.where(A_temp <= 1e-10)] = 1  # Avoid division by 0, due to overflow in exp
 denominator = np.apply_along_axis(lambda ar: ar * particles_slice, 1, A_temp)
 
 # Normalized amplitude spectrum
 A_res = A / denominator
+Phi_res = Phi
+
+# Calculate the synthesized pulse
+A_synth = np.apply_along_axis(lambda ar: ar * particles_target, 1, A_synth) * A_res
+A_synth[np.isnan(A_synth)] = 0
+E_synth = A_synth * np.exp(Phi_res*1j)
+
+# Compare pulses in LOFAR frequency band 30-80MHz
+antenna = 2
+f_check_range = np.logical_and(30 * 1e6 <= freq[f_range], freq[f_range] <= 80 * 1e6)
+f_range = np.logical_and(30 * 1e6 <= freq, freq <= 80 * 1e6)
+
+os.chdir(os.path.join(SIM_DIRECTORY, f'SIM{TARGET_NR}_coreas/'))
+data = np.zeros([2082, 3])
+for file in glob.glob(f'raw_{antenna}x*'):
+    with open(file, 'r') as f:
+        data += (np.genfromtxt(f) * c_vacuum * 1e2)[:, 1:]
+spectrum = np.apply_along_axis(np.fft.rfft, 0, data)
+filtered = spectrum[f_range]
+signal = np.apply_along_axis(np.fft.irfft, 0, filtered)[:, :2]
+
+signal_synth = E_synth[antenna, :, f_check_range, :]
