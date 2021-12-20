64a3a81a40124a35f2f8c8b13d76748f1599117e
Mitja Desmet <mitja.desmet@vub.be>
2021-12-20 13:37:02 +0100

    Added David solution to the plots, as well as phases spectrum plots. Plots are now saved instead of shown.

--- Amplitude spectrum fitting/CompareSliceSynthesis.py
+++ Amplitude spectrum fitting/CompareSliceSynthesis.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-SLICE = 350
+SLICE = 650
 ANTENNA = (1, 3, 5)
 
 SIM_DIRECTORY = '/mnt/hgfs/Shared data/BulkSynth/bulksynth-17/'
+FIG_DIRECTORY = '../Figures/SpectrumComparisons'
 PARAM_DIRECTORY = '/home/mdesmet/PycharmProjects/cr-template-synthesis/Amplitude spectrum fitting/paramProfileFitUB50/'
 F0 = 50
 TEMPLATE_NR = '100001'
@@ -39,12 +40,30 @@ def load_shower_params(shower_nr, path=SIM_DIRECTORY):
     return long[:, 0], np.sum(long[:, 1:], axis=1), x_max
 
 
-def calc_amp_corr(x_max, params, f, d=0.):
+def load_fit_parameters_david(slice_nr):
+    file = '/home/mdesmet/PycharmProjects/davidMWE/ampfit_pars_shape(2, 1, 207, 6, 2, 3, 3)'
+    fit_params = np.loadtxt(file).reshape((2, 1, 207, 6, 2, 3, 3))
+
+    params_x = np.zeros((6, 9))
+    params_y = np.zeros((6, 9))
+
+    params_x[:, :3] = fit_params[0, 0, slice_nr, :, 0, 0, :]
+    params_x[:, 3:6] = fit_params[0, 0, slice_nr, :, 0, 1, :]
+    params_x[:, 6:] = fit_params[0, 0, slice_nr, :, 0, 2, :]
+
+    params_y[:, :3] = fit_params[0, 0, slice_nr, :, 1, 0, :]
+    params_y[:, 3:6] = fit_params[0, 0, slice_nr, :, 1, 1, :]
+    params_y[:, 6:] = fit_params[0, 0, slice_nr, :, 1, 2, :]
+
+    return params_x, params_y
+
+
+def calc_amp_corr(x_max, params, f, d=0., f0=F0):
     a0 = np.polynomial.polynomial.polyval(x_max, params[antenna, :3])
     b = np.polynomial.polynomial.polyval(x_max, params[antenna, 3:6])
     c = np.polynomial.polynomial.polyval(x_max, params[antenna, 6:])
 
-    return a0 * np.exp(b * (f - F0) + c * (f - F0) ** 2) + d
+    return a0 * np.exp(b * (f - f0) + c * (f - f0) ** 2) + d
 
 
 def norm_template(temp_trace, temp_amp_corr, temp_n_slice):
@@ -64,11 +83,14 @@ def filter_pulse(pulse, f_range):
 # Get all the fit parameters from the files
 param_x = np.genfromtxt(os.path.join(PARAM_DIRECTORY, 'fitX', f'slice{SLICE}.dat'))[:, 1:]
 param_y = np.genfromtxt(os.path.join(PARAM_DIRECTORY, 'fitY', f'slice{SLICE}.dat'))[:, 1:]
+param_x_david, param_y_david = load_fit_parameters_david(int(SLICE / 5 - 1))
 d_fit = get_d_fit(SLICE)
 
 # Prepare the figure
-fig_amp, ax = plt.subplots(2, 2, num=1, figsize=(10, 15))
+fig_amp, ax = plt.subplots(2, 2, num=1, figsize=(15, 10))
+fig_phase, ax2 = plt.subplots(2, 2, num=2, figsize=(12, 10))
 ax = ax.flatten()
+ax2 = ax2.flatten()
 
 # Load the shower parameters
 x_slices, template_long, template_max = load_shower_params(TEMPLATE_NR)
@@ -79,6 +101,10 @@ ax[0].plot(x_slices, template_long, c='purple', label='Template')
 ax[0].plot(x_slices, target_long, c='k', label='Real')
 ax[0].legend()
 
+ax2[0].plot(x_slices, template_long, c='purple', label='Template')
+ax2[0].plot(x_slices, target_long, c='k', label='Real')
+ax2[0].legend()
+
 for ind, antenna in enumerate(ANTENNA):
     template = np.genfromtxt(os.path.join(SIM_DIRECTORY, f'SIM{TEMPLATE_NR}_coreas', f'raw_{antenna}x{SLICE}.dat'))
     target = np.genfromtxt(os.path.join(SIM_DIRECTORY, f'SIM{TARGET_NR}_coreas', f'raw_{antenna}x{SLICE}.dat'))
@@ -88,24 +114,57 @@ for ind, antenna in enumerate(ANTENNA):
 
     amp_corr_x = calc_amp_corr(template_max, param_x, freq, d_fit[ind])
     amp_corr_y = calc_amp_corr(template_max, param_y, freq, d_fit[ind])
-    template_amp, template_phase = norm_template(template[:, 1:3], np.stack((amp_corr_x, amp_corr_y)).T,
+    template_amp, template_phase = norm_template(template[:, 1:3],
+                                                 np.stack((amp_corr_x, amp_corr_y)).T,
                                                  template_long[int(SLICE / 5 - 1)])
+
+    amp_corr_x_david = calc_amp_corr(template_max, param_x_david, freq, d_fit[ind], f0=0)
+    amp_corr_y_david = calc_amp_corr(template_max, param_y_david, freq, d_fit[ind], f0=0)
+    template_amp_david, template_phase_david = norm_template(template[:, 1:3],
+                                                             np.stack((amp_corr_x_david, amp_corr_y_david)).T,
+                                                             template_long[int(SLICE / 5 - 1)])
 
     amp_corr_x = calc_amp_corr(target_max, param_x, freq)
     amp_corr_y = calc_amp_corr(target_max, param_y, freq)
     synth_amp = template_amp * np.stack((amp_corr_x, amp_corr_y)).T * target_long[int(SLICE / 5 - 1)]
     synth_phase = template_phase
 
+    amp_corr_x_david = calc_amp_corr(target_max, param_x_david, freq, f0=0)
+    amp_corr_y_david = calc_amp_corr(target_max, param_y_david, freq, f0=0)
+    synth_amp_david = template_amp_david * np.stack((amp_corr_x_david, amp_corr_y_david)).T \
+        * target_long[int(SLICE / 5 - 1)]
+    synth_phase_david = template_phase_david
+
     target_amp, target_phase = norm_template(target[:, 1:3], np.ones(synth_amp.shape), 1.)
 
     ax[ind + 1].plot(freq[freq_range], synth_amp[:, 0][freq_range], c='maroon', linestyle='--')
+    ax[ind + 1].plot(freq[freq_range], synth_amp_david[:, 0][freq_range], c='green', linestyle='--')
     ax[ind + 1].plot(freq[freq_range], target_amp[:, 0][freq_range], c='k', linestyle='--')
-    ax[ind + 1].plot(freq[freq_range], synth_amp[:, 1][freq_range], c='maroon')
-    ax[ind + 1].plot(freq[freq_range], target_amp[:, 1][freq_range], c='k')
+    ax[ind + 1].plot(freq[freq_range], synth_amp[:, 1][freq_range], c='maroon', label='Synthesized')
+    ax[ind + 1].plot(freq[freq_range], synth_amp_david[:, 1][freq_range], c='green', label='David parameters')
+    ax[ind + 1].plot(freq[freq_range], target_amp[:, 1][freq_range], c='k', label='Real')
+    ax[ind + 1].set_xlabel(r'Freq $[MHz]$')
     ax[ind + 1].set_title(f'Antenna {antenna}')
+    ax[ind + 1].legend()
 
-# Set figure parameters
+    ax2[ind + 1].plot(freq[freq_range], synth_phase[:, 0][freq_range], c='maroon', linestyle='--')
+    ax2[ind + 1].plot(freq[freq_range], target_phase[:, 0][freq_range], c='k', linestyle='--')
+    ax2[ind + 1].plot(freq[freq_range], synth_phase[:, 1][freq_range], c='maroon', label='Synthesized')
+    ax2[ind + 1].plot(freq[freq_range], target_phase[:, 1][freq_range], c='k', label='Real')
+    ax2[ind + 1].set_xlim([0, 100])
+    ax2[ind + 1].set_xlabel(r'Freq $[MHz]$')
+    ax2[ind + 1].set_title(f'Antenna {antenna}')
+    ax2[ind + 1].legend()
+
+# Set figure parameters and save
 fig_amp.suptitle(r'Mapping $X_{max}^{temp}$ = ' + str(int(template_max))
                  + r' to $X_{max}^{target}$ = ' + str(int(target_max))
                  + f'\n Amplitude spectra for slice {SLICE} g/cm2')
-plt.show()
+plt.figure(fig_amp)
+plt.savefig(os.path.join(FIG_DIRECTORY, f'AmpSpectrumComparison{SLICE}.png'), bbox_inches='tight')
+
+fig_phase.suptitle(r'Mapping $X_{max}^{temp}$ = ' + str(int(template_max))
+                   + r' to $X_{max}^{target}$ = ' + str(int(target_max))
+                   + f'\n Phase spectra for slice {SLICE} g/cm2')
+plt.figure(fig_phase)
+plt.savefig(os.path.join(FIG_DIRECTORY, f'PhiSpectrumComparison{SLICE}.png'), bbox_inches='tight')
