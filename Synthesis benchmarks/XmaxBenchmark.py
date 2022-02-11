dd78eac0f2fbda7e4b0cabc97e028e1837143eeb
Mitja Desmet <mitja.desmet@vub.be>
2022-02-11 16:28:17 +0100

    Multiple scoring functions for benchmarking

--- Synthesis benchmarks/XmaxBenchmark.py
+++ Synthesis benchmarks/XmaxBenchmark.py
@@ -5,15 +5,34 @@ Tool for automatic benchmarker of synthesis result, as a function of difference 
 import os
 import glob
 import numpy as np
+import matplotlib.pyplot as plt
 from GlobalVars import RANGES_LOFAR as RANGES
+from GlobalVars import FIG_DIRECTORY as FIG
 from GlobalVars import TIME_DELTA, DISTANCES
 from crsynthesizer.FileReaderWriter import working_directory
 
 F_MIN, F_MAX, F0 = RANGES
+FIG_DIR = os.path.join(FIG, 'Benchmarks')
+DISTANCES = np.array(DISTANCES)
 
 
 def amplitude_function(params, frequencies, d_noise=0.):
     return params[0] * np.exp(params[1] * (frequencies - F0) + params[2] * (frequencies - F0) ** 2) + d_noise
+
+
+def filter_pulse(pulse, f_min=F_MIN, f_max=F_MAX, delta=TIME_DELTA):
+    freq = np.fft.rfftfreq(len(pulse), delta) / 1e6  # Freq in MHz
+
+    spectrum = np.fft.rfft(pulse)
+    filtered = spectrum * np.logical_and(freq >= f_min, freq <= f_max)
+
+    return np.fft.irfft(filtered)
+
+
+def fluence(signal, delta=TIME_DELTA):
+    from scipy.constants import c, epsilon_0
+
+    return c * epsilon_0 * delta * np.sum(np.array(signal) ** 2)
 
 
 class Template:
@@ -39,7 +58,6 @@ class Template:
         n_time = test.shape[0]
 
         freq = np.fft.rfftfreq(n_time, TIME_DELTA) / 1e6  # Frequency in MHz
-        # f_range = np.logical_and(freq > F_MIN, freq < F_MAX)
         n_freq = len(freq)
 
         return n_slices, n_antennas, n_pol, n_freq, n_time
@@ -63,7 +81,7 @@ class Template:
 
         return long[:, 0], np.sum(long[:, 1:], axis=1), x_max
 
-    def load_shower_pulse(self):
+    def load_shower_pulse(self, filtered=True):
         """
         Load the radio pulse of the shower in every antenna.
         :return: Time traces in every antenna, with corresponding time values
@@ -73,18 +91,18 @@ class Template:
         x_slices, _, _ = self.load_shower_parameters()
 
         with working_directory(self.raw_dir):
-            n_antennas = int(len(os.listdir('.')) / len(x_slices))
+            times = np.zeros((self.n[1], self.n[4]))  # shape = n_antennas, n_time
+            traces = np.zeros((self.n[1], self.n[2], self.n[4]))  # shape = n_antennas, n_pol, n_time
 
-            test = np.loadtxt('raw_0x5.dat')
-            times = np.zeros((n_antennas, test.shape[0]))  # shape = n_antennas, n_time
-            traces = np.zeros((n_antennas, test.shape[1] - 1, test.shape[0]))  # shape = n_antennas, n_pol, n_time
-
-            for antenna in range(n_antennas):
+            for antenna in range(self.n[1]):
                 times[antenna] = np.genfromtxt(f'raw_{antenna}x5.dat', usecols=(0,))
                 antenna_data = np.zeros(traces.shape[1:])
                 for x_slice in x_slices:
-                    antenna_data += np.loadtxt(f'raw_{antenna}x{int(x_slice)}.dat')[:, 1:].T * c * 1e2
+                    antenna_data += np.loadtxt(f'raw_{antenna}x{int(x_slice)}.dat')[:, 1:(1+self.n[2])].T * c * 1e2
                 traces[antenna] = antenna_data
+
+        if filtered:
+            return np.apply_along_axis(filter_pulse, 2, traces), times
 
         return traces, times
 
@@ -154,7 +172,7 @@ class Template:
 
         for slice_nr in range(self.n[0]):
             for antenna_nr in range(self.n[1]):
-                fit_params = target_params[antenna_nr, slice_nr]
+                fit_params = target_params[slice_nr, antenna_nr]
                 amp_params = np.polynomial.polynomial.polyval(target_x_max, fit_params.T).T
 
                 target_amp = np.apply_along_axis(lambda p: amplitude_function(p, freq), 1, amp_params)
@@ -169,37 +187,97 @@ class Template:
 
 class Benchmark:
     def __init__(self, showers_dir, param_dir, mode="peak_ratio"):
-        self.showers = list(showers_dir)
+        if isinstance(showers_dir, str):
+            self.showers = list([showers_dir])
+        else:
+            self.showers = showers_dir
         self.parameters = param_dir
 
         self.mode = mode
         self.scores = {}
 
-    def select_showers(self):
+    def select_showers(self, k=20):
+        from random import sample
         showers = []
 
         for path in self.showers:
-            showers.extend([Template(name.split('_')[0][3:], path, self.parameters)
-                            for name in glob.glob("SIM*_coreas", root_dir=path)])
+            with working_directory(path):
+                showers.extend([Template(name.split('_')[0][3:], path, self.parameters)
+                                for name in sample(glob.glob("SIM*_coreas"), k)])
 
         return showers
 
     def score_synth(self, synth, real):
         if self.mode == 'peak_ratio':
-            return np.amax(synth) / np.amax(real)
+            return np.amax(synth, axis=2) / np.amax(real, axis=2)
         elif self.mode == 'peak_diff':
             return np.amax(synth) - np.amax(real)
+        elif self.mode == 'energy_ratio':
+            return fluence(synth) / fluence(real)
+        elif self.mode == 'energy_diff':
+            return fluence(synth) - fluence(real)
+        elif self.mode == 'correlation_max':
+            from scipy.signal import correlate, correlation_lags
 
-    def run(self):
-        templates = self.select_showers()
-        targets = self.select_showers()
+            lags = correlation_lags(len(synth), len(real))
+            window = np.logical_and(lags > -100, lags < 100)
+            return np.amax(correlate(synth, real)[window])
+        elif self.mode == 'correlation_shift':
+            from scipy.signal import correlate, correlation_lags
+
+            lags = correlation_lags(len(synth), len(real))
+            window = np.logical_and(lags > -100, lags < 100)
+            return np.argmax(correlate(synth, real)[window])
+        elif self.mode == 'correlation_center':
+            from scipy.signal import correlate, correlation_lags
+
+            lags = correlation_lags(len(synth), len(real))
+            window = np.where(lags == 0)
+            return correlate(synth, real)[window]
+
+    def run(self, number=4):
+        templates = self.select_showers(k=number)
+        targets = self.select_showers(k=number)
 
         for template in templates:
+            template.normalise()
+
             for target in targets:
                 synth = template.map_to_target(target)
-                real = target.load_shower_pulse()
+                real = target.load_shower_pulse()[0]
 
                 _, _, synth_max = template.load_shower_parameters()
                 _, _, real_max = target.load_shower_parameters()
 
                 self.scores[(synth_max, real_max)] = self.score_synth(synth, real)
+
+    def plot_scores(self):
+        import matplotlib.cm as cm
+        import matplotlib.colors as colors
+
+        scores = list(self.scores.values())
+        temp_max, real_max = zip(*self.scores.keys())
+
+        n_antenna, n_pol = scores[0].shape
+
+        for ant in range(n_antenna):
+            antenna_scores = np.array([score[ant] for score in scores])
+
+            cmap = cm.plasma
+            norm = colors.Normalize(vmin=np.amin(antenna_scores), vmax=np.amax(antenna_scores))
+
+            fig, ax = plt.subplots(1, n_pol, num=ant, figsize=(12, 6))
+            for pol in range(n_pol):
+                ax[pol].scatter(temp_max, real_max, c=antenna_scores[:, pol], cmap=cmap, norm=norm)
+                ax[pol].plot([min(temp_max + real_max), max(temp_max + real_max)],
+                             [min(temp_max + real_max), max(temp_max + real_max)],
+                             c='maroon', linestyle='--', linewidth=0.5)
+
+                ax[pol].set_xlabel(r"$X_{max}$ of template")
+                ax[pol].set_ylabel(r"$X_{max}$ of target")
+
+                ax[pol].set_aspect("equal")
+
+            fig.suptitle(f'{self.mode} benchmark for antenna {ant}')
+            fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=ax[:], location='right', shrink=0.7)
+            fig.savefig(os.path.join(FIG_DIR, f'benchmark_{self.mode}_antenna{ant}.png'), bbox_inches='tight')
