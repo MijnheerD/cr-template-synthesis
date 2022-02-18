5f2c28c653170dbd454b139430e3182eec356369
Mitja Desmet <mitja.desmet@vub.be>
2022-02-18 18:07:59 +0100

    Added option for R colouring

--- Amplitude spectrum fitting/AmplitudeFitPlotRL.py
+++ Amplitude spectrum fitting/AmplitudeFitPlotRL.py
@@ -91,8 +91,29 @@ def add_target_to_fig(axis, x_vals, y_vals):
         axis[j].scatter(x_vals[j], y_vals[j], marker='o', facecolors='none', edgecolors='r')
 
 
+def add_labels(x_axes, y_axes):
+    y_labels = [r'$A_0 / N_{slice}$ [$V/ \mu m$]', r'b [$1 / MHz$]', r'c [$1 / MHz^2$]']
+
+    for j, (x, y) in enumerate(zip(x_axes, y_axes)):
+        x.set_xlabel(r'$X_{max}$ [$g/cm^2$]')
+        x.set_ylabel(y_labels[j] + '(x-component)')
+        x.set_title(f'X = {int(X_SLICE)} g/cm^2, r = {int(DISTANCES[ANTENNA] / 100)} m')
+
+        y.set_xlabel(r'$X_{max}$ [$g/cm^2$]')
+        y.set_ylabel(y_labels[j] + '(y-component)')
+        y.set_title(f'X = {int(X_SLICE)} g/cm^2, r = {int(DISTANCES[ANTENNA] / 100)} m')
+
+
+def add_colorbar_to_fig(figs, x_axes, y_axes, colormap, normalisation):
+    couples = list(zip(x_axes, y_axes))
+    for j in range(len(figs)):
+        figs[j].colorbar(cm.ScalarMappable(norm=normalisation, cmap=colormap),
+                         ax=couples[j], location='right', shrink=1)
+
+
 # General parameters
 FIG_DIRECTORY = os.path.join(FIG_DIRECTORY, 'AmplitudeFitRL')
+PLOT_PARAMETER = 'L'
 TARGET = '100000'
 
 if __name__ == "__main__":
@@ -106,26 +127,29 @@ if __name__ == "__main__":
         X_SLICE = 600
         ANTENNA = 3
 
-# Make the figures to plot the parameters
+# Set plotting parameters
 plt.style.use('dark_background')
-fig1, [ax1, ax2] = plt.subplots(1, 2, figsize=(15, 6))
-fig2, [ax3, ax4] = plt.subplots(1, 2, figsize=(15, 6))
-fig3, [ax5, ax6] = plt.subplots(1, 2, figsize=(15, 6))
-
-axX = [ax1, ax3, ax5]
-axY = [ax2, ax4, ax6]
-
 cmap = cm.get_cmap('plasma')
-norm = colors.Normalize(vmin=190, vmax=290, clip=True)
 
 # Construct the required dictionaries and file handlers
 simsRL = sims_r_l(REAS_DIRECTORY)
 simsPART = sims_particles(REAS_DIRECTORY, X_SLICE)
 
-target_x = None
-target_y = None
 fitFiles = ReaderWriter()
-for entry in FIT_DIRECTORIES:
+for E, entry in enumerate(FIT_DIRECTORIES):
+    # Prepare the figures
+    fig1, [ax1, ax2] = plt.subplots(1, 2, figsize=(15, 6))
+    fig2, [ax3, ax4] = plt.subplots(1, 2, figsize=(15, 6))
+    fig3, [ax5, ax6] = plt.subplots(1, 2, figsize=(15, 6))
+
+    axX = [ax1, ax3, ax5]
+    axY = [ax2, ax4, ax6]
+
+    # Target selection
+    target_x = None
+    target_y = None
+
+    # Open all the files to read in the fit parameters
     fitFiles.open_all_files(entry)
 
     parametersX = []
@@ -136,12 +160,18 @@ for entry in FIT_DIRECTORIES:
 
         if simsPART[name][1]:
             R, L, X_max = simsRL[name]
+            if PLOT_PARAMETER == 'R':
+                P = R
+            elif PLOT_PARAMETER == 'L':
+                P = L
+            else:
+                assert ValueError, "Plot parameter not recognized"
 
             lineX = read_fitfile(fileX, X_SLICE, ANTENNA)
             lineY = read_fitfile(fileY, X_SLICE, ANTENNA)
 
-            parametersX.append([X_max, lineX['A'] / simsPART[name][0], lineX['b'], lineX['c'], L, proton])
-            parametersY.append([X_max, lineY['A'] / simsPART[name][0], lineY['b'], lineY['c'], L, proton])
+            parametersX.append([X_max, lineX['A'] / simsPART[name][0], lineX['b'], lineX['c'], P, proton])
+            parametersY.append([X_max, lineY['A'] / simsPART[name][0], lineY['b'], lineY['c'], P, proton])
 
             if name == TARGET:
                 target_x = [X_max] * 6
@@ -152,36 +182,25 @@ for entry in FIT_DIRECTORIES:
     arY = np.array(parametersY)
 
     X = arX[:, 0]
-    L = arX[:, 4]
+    P = arX[:, 4]
+
+    norm = colors.Normalize(vmin=min(P), vmax=max(P), clip=True)
 
     for i, (x_ax, y_ax) in enumerate(zip(axX, axY)):
-        add_par_to_fig(x_ax, X, arX[:, i + 1], arX[:, 5].astype(bool), L, cmap, norm)
-        add_par_to_fig(y_ax, X, arY[:, i + 1], arX[:, 5].astype(bool), L, cmap, norm)
+        add_par_to_fig(x_ax, X, arX[:, i + 1], arX[:, -1].astype(bool), P, cmap, norm)
+        add_par_to_fig(y_ax, X, arY[:, i + 1], arX[:, -1].astype(bool), P, cmap, norm)
+
+    # Add target marker
+    if target_x is not None:
+        add_target_to_fig(axX + axY, target_x, target_y)
+
+    add_labels(axX, axY)
+    add_colorbar_to_fig([fig1, fig2, fig3], axX, axY, cmap, norm)
+
+    # Go to figure directory and save all figures
+    with working_directory(FIG_DIRECTORY):
+        fig1.savefig(f'A0_{PLOT_PARAMETER}_{ANTENNA}x{X_SLICE}_{E}.png', bbox_inches='tight')
+        fig2.savefig(f'b_{PLOT_PARAMETER}_{ANTENNA}x{X_SLICE}_{E}.png', bbox_inches='tight')
+        fig3.savefig(f'c_{PLOT_PARAMETER}_{ANTENNA}x{X_SLICE}_{E}.png', bbox_inches='tight')
 
     fitFiles.close_all_files()
-
-# Add target marker
-if target_x is not None:
-    add_target_to_fig(axX + axY, target_x, target_y)
-
-# Add labels and colorbar to figures
-y_labels = [r'$A_0 / N_{slice}$ [$V/ \mu m$]', r'b [$1 / MHz$]', r'c [$1 / MHz^2$]']
-
-for i, (x_ax, y_ax) in enumerate(zip(axX, axY)):
-    x_ax.set_xlabel(r'$X_{max}$ [$g/cm^2$]')
-    x_ax.set_ylabel(y_labels[i] + '(x-component)')
-    x_ax.set_title(f'X = {int(X_SLICE)} g/cm^2, r = {int(DISTANCES[ANTENNA] / 100)} m')
-
-    y_ax.set_xlabel(r'$X_{max}$ [$g/cm^2$]')
-    y_ax.set_ylabel(y_labels[i] + '(y-component)')
-    y_ax.set_title(f'X = {int(X_SLICE)} g/cm^2, r = {int(DISTANCES[ANTENNA] / 100)} m')
-
-fig1.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=[ax1, ax2], location='right', shrink=1)
-fig2.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=[ax3, ax4], location='right', shrink=1)
-fig3.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap), ax=[ax5, ax6], location='right', shrink=1)
-
-# Go to figure directory and save all figures
-os.chdir(FIG_DIRECTORY)
-fig1.savefig(f'A0_L_{ANTENNA}x{X_SLICE}.png', bbox_inches='tight')
-fig2.savefig(f'b_L_{ANTENNA}x{X_SLICE}.png', bbox_inches='tight')
-fig3.savefig(f'c_L_{ANTENNA}x{X_SLICE}.png', bbox_inches='tight')
